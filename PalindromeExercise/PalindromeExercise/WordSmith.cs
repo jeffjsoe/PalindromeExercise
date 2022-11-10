b1b5a79ba0573b084fe151b8299700953f6ec3e1
Jeffrey Jose <jefjosecool@gmail.com>
2022-11-10 15:32:51 -0500

    "Finished'

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+
+        //Hello
+        public bool IsAPalindrome(string p)
+        {
+            string empty = "";
+            for(int i =p.Length-1;i>=0;i--)
+            {
+                empty = empty + p[i];
+
+
+            }
+
+            if (empty == p)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+﻿using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,9 +6,22 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+
+        //[InlineData()] // ← you will need to put some test data and expected result here.  
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("car", false)]
+        public void Test1(string word,bool expected)
         {
+
+            var tester = new WordSmith();
+
+            var total=tester.IsAPalindrome(word);
+
+            Assert.Equal(expected, total);
+
+
+
 
         }
     }
